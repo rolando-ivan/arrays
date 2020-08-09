90f3b58a9257336d435da14e92949cbbbdc55b6f
rolan <rolandoleiva2201@gmai.com>
2020-08-09 18:47:09 -0300

    hola message

--- app/src/main/java/com/example/ivan/MainActivity.java
+++ app/src/main/java/com/example/ivan/MainActivity.java
@@ -55,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
             timepo += intervalo;*/
 
            totalNombre += "\n"+ item;
+           String hola =  "hols";
         }
 
         textViewListaDeUsuarios.setText(totalNombre);
