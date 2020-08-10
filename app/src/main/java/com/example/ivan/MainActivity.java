2543c87efe96c55a03e93f1e3d038e424fb54d57
rolan <rolandoleiva2201@gmai.com>
2020-08-10 18:33:48 -0300

    hola message

--- app/src/main/java/com/example/ivan/MainActivity.java
+++ app/src/main/java/com/example/ivan/MainActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText edit_text_nombres_para_lista;
+    private ImageView agregar, quitar;
     private TextView textViewListaDeUsuarios;
     private ArrayList<String> listaDeNombres;
     //private int index =0 ;
@@ -22,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textViewListaDeUsuarios = findViewById(R.id.textView);
+        edit_text_nombres_para_lista = findViewById(R.id.editText);
+        agregar = findViewById(R.id.imagen_agregar);
+        quitar = findViewById(R.id.imagen_quitar);
 
         String nombre1 = "Juan";
 
@@ -30,15 +36,18 @@ public class MainActivity extends AppCompatActivity {
         listaDeNombres.add("Pedro");
         listaDeNombres.add("Leiva");
         listaDeNombres.add("Rolando");
-        listaDeNombres.remove(nombre1);
-        //listaDeNombres.remove("Juan"); No funciona porque la bariable la crea la computadora
+        /*listaDeNombres.remove(nombre1);
+        //listaDeNombres.remove("Juan"); No funciona porque la bariable la crea la computadora*/
     }
 
     public void mostrar (View view){
         /*final int intervalo = 1000;
         int timepo = 0;*/
+        String nombre2 = edit_text_nombres_para_lista.getText().toString();
+        listaDeNombres.add(nombre2);
+        edit_text_nombres_para_lista.setText("");
 
-        String totalNombre="";
+        String totalNombre = "" ;
 
         for (String item : listaDeNombres){
 /* new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
@@ -51,8 +60,18 @@ public class MainActivity extends AppCompatActivity {
 
             timepo += intervalo;*/      totalNombre += "\n" + item;
         }
-
         textViewListaDeUsuarios.setText(totalNombre);
+    }
+    public void retirar(View view){
+        String nombre = edit_text_nombres_para_lista.getText().toString();
+        listaDeNombres.remove(nombre);
+        edit_text_nombres_para_lista.setText("");
 
+        String totalNombre = "" ;
+
+        for (String item : listaDeNombres){
+            totalNombre += "\n" + item;
+        }
+        textViewListaDeUsuarios.setText(totalNombre);
     }
 }
