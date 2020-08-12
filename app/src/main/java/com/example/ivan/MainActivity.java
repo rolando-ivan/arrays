d11833f263dd8fc94b60fb67eabb844669388558
rolan <rolandoleiva2201@gmai.com>
2020-08-12 11:55:31 -0300

    hola message

--- app/src/main/java/com/example/ivan/MainActivity.java
+++ app/src/main/java/com/example/ivan/MainActivity.java
@@ -3,125 +3,98 @@ package com.example.ivan;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Button;
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText edit_text_nombres_para_lista, edit_text_retirar_nombre_lista;
-    private Button agregar, quitar;
-    private Button agregar_lista, retirar_lista;
-    private ImageView barra;
     private TextView textViewListaDeUsuarios;
     private ArrayList<String> listaDeNombres;
-    long AnimationDuration = 1500;
-    //private int index =0 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        agregar_lista = findViewById(R.id.agregar_lista);
-        retirar_lista = findViewById(R.id.retirar_lista);
+        final LinearLayout menu = findViewById(R.id.menu);
+        final ImageView menu1 = findViewById(R.id.menu1);
         textViewListaDeUsuarios = findViewById(R.id.textView);
         edit_text_nombres_para_lista = findViewById(R.id.editTextAgregar);
         edit_text_retirar_nombre_lista = findViewById(R.id.editTextRetirar);
-        barra =findViewById(R.id.barra);
         final ConstraintLayout contenedor = findViewById(R.id.contenedor);
-        agregar = findViewById(R.id.imagen_agregar);
+        Button agregar = findViewById(R.id.imagen_agregar);
         agregar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*ObjectAnimator animacionXA = ObjectAnimator.ofFloat(agregar, "x", 735f);
-                ObjectAnimator animacionXQ = ObjectAnimator.ofFloat(quitar, "x", 630f);
-                ObjectAnimator animacionXB = ObjectAnimator.ofFloat(barra, "x", 710f);
-                ObjectAnimator animacionXEditTextQuitar = ObjectAnimator.ofFloat(edit_text_retirar_nombre_lista, "x", 800f);
-                ObjectAnimator animacionXEditTextAgregar = ObjectAnimator.ofFloat(edit_text_nombres_para_lista, "x", 100f);
-                ObjectAnimator animacionXBotonQuitar = ObjectAnimator.ofFloat(retirar_lista, "x", 950f);
-                ObjectAnimator animacionXBotonAgregar = ObjectAnimator.ofFloat(agregar_lista, "x", 255f);
-                animacionXA.setDuration(AnimationDuration);
-                animacionXQ.setDuration(AnimationDuration);
-                animacionXB.setDuration(AnimationDuration);
-                animacionXBotonQuitar.setDuration(AnimationDuration);
-                animacionXEditTextQuitar.setDuration(AnimationDuration);
-                animacionXEditTextAgregar.setDuration(AnimationDuration);
-                animacionXBotonAgregar.setDuration(AnimationDuration);
-                AnimatorSet animatorSet = new AnimatorSet();
-                animatorSet.playTogether(animacionXA, animacionXQ, animacionXB, animacionXBotonQuitar, animacionXEditTextQuitar,
-                        animacionXEditTextAgregar, animacionXBotonAgregar);
-                animatorSet.start();*/
-
-            }
-        });
-        quitar = findViewById(R.id.imagen_quitar);
-        quitar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /*ObjectAnimator animacionXQ = ObjectAnimator.ofFloat(quitar, "x", -80f);
-                ObjectAnimator animacionXA = ObjectAnimator.ofFloat(agregar, "x", 20f);
-                ObjectAnimator animacionXB = ObjectAnimator.ofFloat(barra, "x", 0f);
-                ObjectAnimator animacionXEditTextQuitar = ObjectAnimator.ofFloat(edit_text_retirar_nombre_lista, "x", 100f);
-                ObjectAnimator animacionXEditTextAgregar = ObjectAnimator.ofFloat(edit_text_nombres_para_lista, "x", -600f);
-                ObjectAnimator animacionXBotonQuitar = ObjectAnimator.ofFloat(retirar_lista, "x", 255f);
-                ObjectAnimator animacionXBotonAgregar = ObjectAnimator.ofFloat(agregar_lista, "x", -450f);
-                animacionXQ.setDuration(AnimationDuration);
-                animacionXA.setDuration(AnimationDuration);
-                animacionXB.setDuration(AnimationDuration);
-                animacionXBotonQuitar.setDuration(AnimationDuration);
-                animacionXEditTextQuitar.setDuration(AnimationDuration);
-                animacionXEditTextAgregar.setDuration(AnimationDuration);
-                animacionXBotonAgregar.setDuration(AnimationDuration);
-                AnimatorSet animatorSet = new AnimatorSet();
-                animatorSet.playTogether(animacionXQ, animacionXA, animacionXB, animacionXBotonQuitar, animacionXEditTextQuitar,
-                        animacionXEditTextAgregar, animacionXBotonAgregar);
-                animatorSet.start();*/
-                ObjectAnimator animacionX = ObjectAnimator.ofFloat(contenedor, "x", -710f);
+                ObjectAnimator animacionX = ObjectAnimator.ofFloat(contenedor, "x", 0f);
                 animacionX.setDuration(2000);
                 AnimatorSet animatorSet = new AnimatorSet();
                 animatorSet.playTogether(animacionX);
                 animatorSet.start();
             }
         });
-
-        //String nombre1 = "Juan";
-
+        Button quitar = findViewById(R.id.imagen_quitar);
+        quitar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ObjectAnimator animacionX = ObjectAnimator.ofFloat(contenedor, "x", -716f);
+                animacionX.setDuration(2000);
+                AnimatorSet animatorSet = new AnimatorSet();
+                animatorSet.playTogether(animacionX);
+                animatorSet.start();
+            }
+        });
+        ImageView imagen_menu = findViewById(R.id.foto_menu);
+        imagen_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                menu1.setVisibility(View.VISIBLE);
+                ObjectAnimator animacionA = ObjectAnimator.ofFloat(menu1, "alpha", 0f, 1f);
+                ObjectAnimator animacionX = ObjectAnimator.ofFloat(menu, "x", 0f);
+                animacionA.setDuration(2500);
+                animacionX.setDuration(1000);
+                AnimatorSet animatorSet = new AnimatorSet();
+                animatorSet.playTogether(animacionX, animacionA);
+                animatorSet.start();
+            }
+        });
+        menu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ObjectAnimator animacionA = ObjectAnimator.ofFloat(menu1, "alpha", 1f, 0f);
+                ObjectAnimator animacionX = ObjectAnimator.ofFloat(menu, "x", -500f);
+                animacionA.setDuration(500);
+                animacionX.setDuration(1500);
+                AnimatorSet animatorSet = new AnimatorSet();
+                animatorSet.playTogether(animacionX, animacionA);
+                animatorSet.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        menu1.setVisibility(View.GONE);
+                    }
+                });
+                animatorSet.start();
+            }
+        });
         listaDeNombres = new ArrayList<>();
-        /*listaDeNombres.add(nombre1);
-        listaDeNombres.add("Pedro");
-        listaDeNombres.add("Leiva");
-        listaDeNombres.add("Rolando");
-        listaDeNombres.remove(nombre1);
-        //listaDeNombres.remove("Juan"); No funciona porque la bariable la crea la computadora*/
     }
-
     public void mostrar (View view){
-        /*final int intervalo = 1000;
-        int timepo = 0;*/
         String nombre2 = edit_text_nombres_para_lista.getText().toString();
         listaDeNombres.add(nombre2);
         edit_text_nombres_para_lista.setText("");
-
         StringBuilder totalNombre = new StringBuilder();
-
         for (String item : listaDeNombres){
-/* new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    textViewListaDeUsuarios.setText(listaDeNombres.get(index));
-                    index++;
-                }
-            }, timepo);
-
-            timepo += intervalo;*/      totalNombre.append("\n").append(item);
+            totalNombre.append("\n").append(item);
         }
         textViewListaDeUsuarios.setText(totalNombre.toString());
     }
@@ -129,9 +102,7 @@ public class MainActivity extends AppCompatActivity {
         String nombre = edit_text_retirar_nombre_lista.getText().toString();
         listaDeNombres.remove(nombre);
         edit_text_retirar_nombre_lista.setText("");
-
         StringBuilder totalNombre = new StringBuilder();
-
         for (String item : listaDeNombres){
             totalNombre.append("\n").append(item);
         }
